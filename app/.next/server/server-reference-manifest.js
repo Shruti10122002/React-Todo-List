adbaaa75230c2d0c90746ccffdaff14484f5bd15
Shruti10122002 <shrutiranjan2892002@gmail.com>
2024-03-05 19:08:00 +0530

    first commit

---
+++ app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DTAlqH+pgNrDyOPr/JUc3APkxxloLT54Dx3jnpz8eeA=\"\n}"
---
+++ app/page.js
@@ -0,0 +1,69 @@
+"use client"
+import React, {useState} from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setmainTask] = useState([])
+  const submitHandler =(e)=>{
+    e.preventDefault()
+    setmainTask([...mainTask,{ title,desc }]);
+    
+    settitle("")
+    setdesc("")
+    console.log(mainTask)
+    
+
+  }
+  let renderTask =<h2>No Task Available</h2>
+  renderTask= mainTask.map((t,i)=>{
+    return (
+     <div className="flex justify-between" >
+      <h5>{t.title}</h5>
+      <h6>{t.desc}</h6>
+    </div>
+  );
+  });
+  return (
+    <>
+    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center' >
+      Shruti's Todo List</h1>
+      <form onSubmit={submitHandler}>
+        <input type="text" className="text-2xl border-zinc-800 border-2 m-8 px-5 py-2"
+        placeholder="Enter Title Here"
+        value={title}
+        onChange={(e)=>{
+          settitle(e.target.value)
+        }}
+        />
+        </form>
+
+        <form onSubmit={submitHandler}>
+      
+        <input type="text" className='text-2xl border-zinc-800 border-2 m-8 px-5 py-2 '
+        placeholder="Enter Description Here"
+        value={desc}
+        onChange ={(e)=>{
+          setdesc(e.target.value)
+        }}
+        />
+        <button className="bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5">Add Task</button>
+        
+      </form>
+      <hr />
+      <div className="p-8 bg-slate-200">
+        <ul>
+          {renderTask}
+          </ul>
+
+      </div>
+      
+        
+      
+    </>
+    
+  
+  )
+}
+
+export default page
